7b88e02680f07d8d114b9043f06cced1fa05abec
jswittenborn <johnswittenborn@gmail.com>
2020-07-06 18:02:01 -0400

    update

--- app.py
+++ app.py
@@ -29,7 +29,20 @@ def tasks():
     for task in tasks:
         item = {
             "id": task.id,
-            "description": task.description
+            "host_id": task.host_id,
+            "host_name": task.host_name,
+            "neighbourhood": task.neighbourhood,
+            "latitude": task.latitude,
+            "longitude": task.longitude,
+            "room_type": task.room_type,
+            "price": task.price,
+            "minimum_nights": task.minimum_nights,
+            "number_of_reviews": task.number_of_reviews,
+            "last_review": task.last_review,
+            "reviews_per_month": task.reviews_per_month,
+            "calculated_host_listings_count": task.calculated_host_listings_count,
+            "availability_365": task.availability_365,
+            "name": task.name
         }
         data.append(item)
     return jsonify(data)
