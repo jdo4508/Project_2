4bfa6acb7368342fef07018f10b73b7663240c97
unknown <jdo4508@gmail.com>
2020-07-10 19:37:31 -0400

    final map code

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,20 +1,17 @@
+
 var colorScale;
 
-
-
-
-//d3.csv("http://data.insideairbnb.com/united-states/nc/asheville/2020-05-29/visualisations/listings.csv", function(response) {
-d3.json("/listings", function(response) {
-//d3.json("/listings").then((response) => {
-   //console.log(response);
-
+// d3.csv("http://data.insideairbnb.com/united-states/nc/asheville/2020-05-29/visualisations/listings.csv", function(response) {
+    // console.log(response);
+      
+  d3.json("/listings", function(response) {
     colorScale = d3.scaleOrdinal(d3.schemeCategory10);
 
     var radiusScale = d3.scaleLinear()
       .domain([0, d3.max (response, function(listings) { return +listings.price; })])
       .range([1, 10]);
 
-      var geoJSONListingsFeatures = [];  
+      var listingFeatures = [];  
 
       response.forEach(function(listings, i) {
         var info = "<span style='color:" + colorScale(listings.room_type) + "'><b>" +
@@ -22,7 +19,13 @@ d3.json("/listings", function(response) {
                     "room type: <b>" + listings.room_type + "</b>" +  "</b></span><br/>" +
                     "price: <b>" + "$" + listings.price + "</b>" + "</b></span><br/>";
 
-       
+      response.forEach (function(listings, i) {
+        var type = listings.room_type;
+        var name = listings.name;
+        var price = listings.price;
+        
+
+        })
 
         var geoJSONFeatures = {
                       "type": "Feature",
@@ -35,79 +38,122 @@ d3.json("/listings", function(response) {
                           "coordinates": [ +listings.longitude, +listings.latitude ] 
                       }
                   };
-                  geoJSONListingsFeatures.push(geoJSONFeatures);
+                  listingFeatures.push(geoJSONFeatures);
               });
 
-              makeListingMap(geoJSONListingsFeatures);
+              makeListingMap(listingFeatures);
           });   
           
+      
+                  // Creating map object
+        var makeListingMap = function(listingFeatures) {
+
+        var myMap = L.map("map-container", {
+            center: [35.54, -82.55],
+            zoom: 12,
+          });
+            
+        // Adding tile layers to the map
+        var light = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+            attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+            tileSize: 512,
+            maxZoom: 18,
+            zoomOffset: -1,
+            id: 'mapbox/light-v10',
+            accessToken: API_KEY
           
+          });  
+
+        var dark = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+          attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+          tileSize: 512,
+          maxZoom: 18,
+          zoomOffset: -1,
+          id: 'mapbox/dark-v10',
+          accessToken: API_KEY
+        
+        });
+
+        var satellite = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+          attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+          tileSize: 512,
+          maxZoom: 18,
+          zoomOffset: -1,
+          id: 'mapbox/satellite-v9',
+          accessToken: API_KEY
+        
+        }); 
+
+        var streets = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+          attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+          tileSize: 512,
+          maxZoom: 18,
+          zoomOffset: -1,
+          id: 'mapbox/streets-v11',
+          accessToken: API_KEY
+
+        }).addTo(myMap);
+
+      
+
+      L.geoJson(listingFeatures, {
+        style: function (feature) {
+            return {
+                color:       '#000',
+                opacity:     0,
+                radius:      (feature.properties.capacity = 10),
+                fillColor:   feature.properties.color,
+                fillOpacity: 0.7
+            };
+        },
+        onEachFeature: function (feature, layer) {
+            layer.bindPopup(feature.properties.info);
+        },
+        pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng);
+        }
+        
+        }).addTo(myMap);
 
 
-    // Creating map object
-    var makeListingMap = function(geoJSONListingsFeatures) {
+        var overlayMaps = {
+          "Streets": streets,
+          "Grayscale": light,
+          "Dark": dark,
+          "Satellite": satellite
+      };
 
-    var myMap = L.map("map-container", {
-      center: [35.54, -82.55],
-      zoom: 12
-    });
+      L.control.layers(overlayMaps).addTo(myMap);
 
-    // Adding tile layer to the map
-    var tileLayer = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-      attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-      tileSize: 512,
-      maxZoom: 18,
-      zoomOffset: -1,
-      id: 'mapbox/streets-v11',
-      accessToken: API_KEY
-    }).addTo(myMap);
+     
+    // Add legend with d3
+    var legendWidth  = 110,
+    legendHeight = 80;
 
-    L.geoJson(geoJSONListingsFeatures, {
-      style: function (feature) {
-          return {
-              color:       '#000',
-              opacity:     0,
-              radius:      (feature.properties.capacity = 3),
-              fillColor:   feature.properties.color,
-              fillOpacity: 0.5
-          };
-      },
-      onEachFeature: function (feature, layer) {
-          layer.bindPopup(feature.properties.info);
-      },
-      pointToLayer: function (feature, latlng) {
-          return L.circleMarker(latlng);
-      }
+    var legend = d3.select('#map-legend').append('svg')
+      .attr('width', legendWidth)
+      .attr('height', legendHeight);
 
-  }).addTo(myMap);
+    var legends = legend.selectAll(".legend")
+      .data(colorScale.domain())
+    .enter().append("g")
+      .attr("class", "legend")
+      .attr("transform", function(d, i) { return "translate(0," + i * 20
+      + ")"; });
 
-  // Add legend with d3
-  var legendWidth  = 110,
-  legendHeight = 80;
+    // draw legend colored rectangles
+    legends.append("rect")
+      .attr("x", legendWidth - 18)
+      .attr("width", 18)
+      .attr("height", 18)
+      .style("fill", colorScale);
 
-var legend = d3.select('#map-legend').append('svg')
-  .attr('width', legendWidth)
-  .attr('height', legendHeight);
-
-var legends = legend.selectAll(".legend")
-  .data(colorScale.domain())
-.enter().append("g")
-  .attr("class", "legend")
-  .attr("transform", function(d, i) { return "translate(0," + i * 20
-   + ")"; });
-
-// draw legend colored rectangles
-legends.append("rect")
-  .attr("x", legendWidth - 18)
-  .attr("width", 18)
-  .attr("height", 18)
-  .style("fill", colorScale);
-
-// draw legend text
-legends.append("text")
-  .attr("x", legendWidth - 20)
-  .attr("y", 9)
-  .attr("dy", ".35em")
-  .style("text-anchor", "end")
-  .text(function(d) { return d.toLowerCase(); })
-};
+    // draw legend text
+    legends.append("text")
+      .attr("x", legendWidth - 20)
+      .attr("y", 9)
+      .attr("dy", ".35em")
+      .style("text-anchor", "end")
+      .text(function(d) { return d.toLowerCase(); })
+      
+  };
